e93e35f2ce3a7c098e9ef4376dece36b96256aa1
Steven <Sleyva1991@outlook.com>
2019-07-16 16:02:14 -0400

    finished the project, has a bug

--- SwiftVocabulary/DefinitionViewController.swift
+++ SwiftVocabulary/DefinitionViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
     
+    var vocabWord: VocabularyWord? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var definitionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
+   
+    }
+    func updateViews() {
+        guard let vocabWord = vocabWord, isViewLoaded else { return }
+        
+        definitionTextView.text = vocabWord.definition
+        wordLabel.text = vocabWord.word
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ SwiftVocabulary/VocabularyController.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyController.swift
+//  SwiftVocabulary
+//
+//  Created by Steven Leyva on 7/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class VocabularyController {
+    
+    var vocabWords : [VocabularyWord] = [VocabularyWord(word:"Struct" , definition: "Structures are general-purpose, flexible constructs that become the building blocks of your program’s code. You define properties and methods to add functionality to your structures and classes using the same syntax you use to define constants, variables, and functions.")]
+    
+    }
+  
+
